a4608279b768f9bf31f2c56867c1c08f966630da
unknown <jonathanaffre@gmail.com>
2014-04-14 16:12:00 +0200

    Added Ui func

--- public/main.js
+++ public/main.js
@@ -4,6 +4,7 @@
 yepnope({
     load: {
         jquery: 'javascripts/libs/vendors/jquery.js',
+        jqueryUi: 'javascripts/libs/vendors/jqueryUi/js/jquery-ui.js',
         bootstrap: 'javascripts/libs/vendors/bootstrap/js/bootstrap.js',
         underscore: 'javascripts/libs/vendors/underscore.js',
         backbone: 'javascripts/libs/vendors/backbone.js',
@@ -18,21 +19,51 @@ yepnope({
 
 
         //Routes
-        routes: 'routes.js'
+        routes: 'routes.js',
 
+        //UI CMS
+        UI: 'javascripts/UI.js'
     },
 
     callback: {
         "routes": function() {
-            console.log("routes loaded ...");
+
         }
     },
     complete: function() {
         $(function() {
-            $('.carousel').carousel({
-                interval: 4000
+            $(".subMenu").hide();
+            $(".showSettings").click(function(){
+                $(".subMenu").show('blind',200);
+                var action = $(this).data("action");
+                switch(action){
+                    case "pages":
+                        addSettingsPage();
+                        break;
+                    case "elements":
+                        addSettingsElement();
+                        break;
+                    case "settings":
+                        addSettings();
+                        break;
+                }
+                $('.subMenu').append('<a href="#" class="hideSubmenu pull-right">Hide</a>');
+            });
+            $("body").delegate(".hideSubmenu","click",function(){
+                $(".subMenu").html("");
+                $(".subMenu").hide('blind',200);
+            });
+            $("body").delegate(".showWidgets","click",function(){
+                showWidgetMenu();
+                $('.subMenu').append('<a href="#" class="hideSubmenu pull-right">Hide</a>');
+            });
+            $("body").delegate(".backToElement","click",function(){
+                addSettingsElement();
+                $('.subMenu').append('<a href="#" class="hideSubmenu pull-right">Hide</a>');
             })
-
+            //Hide loading when all loads finished
+            $(".contentLoader").hide();
         });
     }
 });
+
